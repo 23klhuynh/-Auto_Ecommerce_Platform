ed640469ef52b94d3d5538eb6610370c12ab6382
Khang Huynh <23klhuynh@gmail.com>
2024-09-10 22:58:12 -0400

    Implement a navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+        <div className="navbar__margin">
+          <ul className="navbar__items">
+            <li className="navbar__item"><Link to="/">HOME</Link></li>
+            <li className="navbar__item"><Link to="/">SHOP</Link></li>
+            <li className="navbar__item"><Link to="/">FINDER</Link></li>
+            <li className="navbar__item"><Link to="/">CONTACT</Link></li>
+          </ul>
+        
+          <p>logo</p>
+          
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
