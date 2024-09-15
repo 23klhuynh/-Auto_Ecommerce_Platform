4ed0650a1f840e3d1190ecae6f5b53b2de0e2781
Khang Huynh <23klhuynh@gmail.com>
2024-09-14 23:58:57 -0400

    Implament a responsive login section

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -4,8 +4,6 @@ import { useNavigate } from "react-router-dom";
 import LoginImg from "../assets/LoginImg.jpg";
 import { Link } from "react-router-dom";
 
-
-
 function RegisterForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -37,46 +35,48 @@ function RegisterForm() {
   return (
     <div className="register">
       <div className="register__section">
-        <div className="register__box">
-          <div className="register__header">
+      <div className="register__header">
             <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold">
               Sign up
             </h1>
           </div>
+        <div className="register__box">
           <form onSubmit={handleSubmit} className="register__form">
             <input
               type="email"
+              required
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Email!"
             />
             <input
               type="password"
+              required
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Password!"
             />
             <input
               type="password"
+              required
               value={comfirmPassword}
               onChange={(e) => setComfirmPassword(e.target.value)}
               placeholder="Confirmed!"
             />
           </form>
           <div className="register__btns">
-            {/* <button className="login__btn" onClick={(e) => handleSubmit(e)}>
-              Login
-              </button> */}
-            <p className="register__recommend">
-              Already a Member? <Link className="to-blue-900">Sign In</Link>
-            </p>
+            <div className="register__footer">
+              <p className="register__recommend text-xs sm:text-xs md:text-sm lg:text-md xl:text-xl">
+                Already a Member? <Link className="text-blue-700">Sign In</Link>
+              </p>
 
-            <button
-              className="register__btn"
-              onClick={(e) => handleSubmit(e)}
-            >
-              Sign up
-            </button>
+              <button
+                className="register__btn"
+                onClick={(e) => handleSubmit(e)}
+              >
+                Sign up
+              </button>
+            </div>
 
             {message && (
               <span className="btn text-red-400 flex justify-center">
