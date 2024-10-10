9dde63f95ff229446a71cb30b15a8e472ebf1f85
Khang Huynh <23klhuynh@gmail.com>
2024-10-09 23:55:17 -0400

    implament the logo for the cars and work on the responsive navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,35 +1,56 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross1 } from "react-icons/rx";
 import { useNavigate } from "react-router-dom";
-
 
 function Navbar({ isScrolled }) {
   const navigate = useNavigate();
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
     <header>
-      <nav className={`navbar ${isScrolled ? "" : "nav-default"}`}>
-        <div className="navbar__margin">
-          <button className="navbar__mobile">
-            <GiHamburgerMenu />
-          </button>
-          <ul className="navbar__items">
-            <li className="navbar__item">
-              <Link to="/dashboard">HOME</Link>
-            </li>
-            <li className="navbar__item">
-              <Link to="/dashboard/shop">SHOP</Link>
-            </li>
-            <li className="navbar__item">
-              <Link to="/dashboard/finder">FINDER</Link>
-            </li>
-            <li className="navbar__item">
-              <Link to="/dashboard/contact">CONTACT</Link>
-            </li>
-          </ul>
+      <nav
+        className={`navbar ${isScrolled ? "" : "nav-default"} ${
+          isClicked ? "click" : ""
+        }`}
+      >
+        <div className={`navbar__container ${
+          isClicked ? "click" : ""
+        }`}>
+          <div className="navbar__margin">
+            <button
+              className="navbar__mobile"
+              onClick={() => setIsClicked(!isClicked)}
+            >
+              {isClicked ? <RxCross1 /> : <GiHamburgerMenu />}
+            </button>
+            <ul className="navbar__items">
+              <li className="navbar__item">
+                <Link to="/dashboard">HOME</Link>
+              </li>
+              <li className="navbar__item">
+                <Link to="/dashboard/shop">SHOP</Link>
+              </li>
+              <li className="navbar__item">
+                <Link to="/dashboard/finder">FINDER</Link>
+              </li>
+              <li className="navbar__item">
+                <Link to="/dashboard/contact">CONTACT</Link>
+              </li>
+            </ul>
 
-          <button className="navbar__btn" onClick={()=>navigate("/login")}>Logout</button>
+            <button className="navbar__btn" onClick={() => navigate("/login")}>
+              Logout
+            </button>
+          </div>
+          {isClicked && (
+            <ul>
+              <li>home</li>
+              <li>home</li>
+              <li>home</li>
+            </ul>
+          )}
         </div>
       </nav>
     </header>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -121,16 +121,15 @@ function Home() {
 
       </section> */}
 
-      <section className="brand">
-        <div className="brand__heading">
+      <section className="car">
+        <div className="car__heading">
         <h1>Popular Car Brand</h1>
         </div>
-        <ul className="car__brand">
+        <ul className="car__brands">
           {carBrand.popular_car_brands.map((brand, index)=>(
-            <li key={index}>
+            <li key={index} className="car__brand">
               <img src={brand} alt="" />
             </li>
-            /* "BMW","Mercedes-Benz","Nissan","Volkswagen","Hyundai" */
           ))}
         </ul>
       </section>
