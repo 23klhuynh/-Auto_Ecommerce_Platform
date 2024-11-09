63a0066188261302dec2a8287a6a3cd7401f58c7
Khang Huynh <23klhuynh@gmail.com>
2024-11-08 23:45:26 -0500

    Adding the responsiveness to the finder page.

--- frontend/src/pages/Finder.jsx
+++ frontend/src/pages/Finder.jsx
@@ -56,9 +56,48 @@ function Finder() {
               <option value="option3">Option 3</option>
             </select>
           </div>
+          <div className="finder__btn">
+            <button className="">Submit</button>
+          </div>
         </div>
 
+        {/* mobile */}
+        <div className="finder__mobile">
+          <div className="mobile__container">
+            <div className="mobile__filter">
+              <button>Filter</button>
+            </div>
+            <span className="mobile__vert-line"></span>
+            <div className="mobile__btn">
+              <button>b1</button>
+              <button>b2</button>
+              <button>b3</button>
+              <button>b4</button>
+              <button>b5</button>
+            </div>
+          </div>
+        </div>
+        {/* mobile */}
+
         <div className="finder__contents">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            voluptatem quae delectus in. Maiores ipsam, quidem praesentium
+            deserunt commodi quaerat labore, repudiandae corrupti nobis
+            blanditiis aliquam laudantium cum fuga repellendus?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            voluptatem quae delectus in. Maiores ipsam, quidem praesentium
+            deserunt commodi quaerat labore, repudiandae corrupti nobis
+            blanditiis aliquam laudantium cum fuga repellendus?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            voluptatem quae delectus in. Maiores ipsam, quidem praesentium
+            deserunt commodi quaerat labore, repudiandae corrupti nobis
+            blanditiis aliquam laudantium cum fuga repellendus?
+          </p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
             voluptatem quae delectus in. Maiores ipsam, quidem praesentium
